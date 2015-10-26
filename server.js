9a3de7b1dc868b2a1cd030e1e228e1e0336e1482
letrungkien0210 <letrungkien0210@gmail.com>
2015-10-26 22:48:54 +0700

    Init

---
+++ server.js
@@ -0,0 +1,25 @@
+//grab express
+var express= require('express');
+
+//create an express app
+var app =express();
+
+// CONFIGURE THE APP
+// ==================================================
+// tell node where to look for site resources
+app.use(express.static(__dirname + '/public'));
+
+//set the view engine to ejs
+app.set('view engine','ejs');
+app.set('port', (process.env.PORT || 8080));
+
+//Create an express route for the home page
+app.get('/', function(req, res){
+	res.sendfile('index.html');
+});
+
+// START THE SERVER
+// ==================================================
+app.listen(app.get('port'), function () {
+	console.log('Node app is running on port', app.get('port'));
+});
