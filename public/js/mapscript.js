742d8d15f23348eac5c0a947844bfc234d1294e9
lva244 <lvanh24494@gmail.com>
2015-11-07 10:35:42 +0700

    Update direction in multi points

--- public/js/mapscript.js
+++ public/js/mapscript.js
@@ -1,10 +1,25 @@
+var labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+var labelIndex = 0;
 var locate;
 var markers = [];
+var info_for_markers = [];
+var address_for_markers = [];
+var waypts = [];
+var map;
+var place;
+var address;
+var directionsDisplay;
+var directionsService;
+var save_response;
+
 function initMap() {
-  var map = new google.maps.Map(document.getElementById('map'), {
+  directionsDisplay = new google.maps.DirectionsRenderer;
+  directionsService = new google.maps.DirectionsService;
+  map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: 10.767523, lng: 106.706772},
     zoom: 13
   });
+    directionsDisplay.setMap(map);
   var input = /** @type {!HTMLInputElement} */(
       document.getElementById('pac-input'));
 
@@ -24,11 +39,13 @@ function initMap() {
   autocomplete.addListener('place_changed', function() {
     infowindow.close();
     marker.setVisible(false);
-    var place = autocomplete.getPlace();
-      locate= place.geometry.location;
+    place = autocomplete.getPlace();
     if (!place.geometry) {
       window.alert("Autocomplete's returned place contains no geometry");
       return;
+    }
+    else {
+        locate= place.geometry.location;
     }
 
     // If the place has a geometry, then present it on a map.
@@ -48,7 +65,7 @@ function initMap() {
     marker.setPosition(place.geometry.location);
     marker.setVisible(true);
 
-    var address = '';
+    address = '';
     if (place.address_components) {
       address = [
         (place.address_components[0] && place.address_components[0].short_name || ''),
@@ -68,30 +85,81 @@ function initMap() {
   } 
 
   setupClickListener('changetype-all', []);
-    
-    
-    // Adds a marker to the map and push to the array.
-    function addMarker(location) {
-      var marker = new google.maps.Marker({
-        position: location,
-        map: map
-      });
-      markers.push(marker);
-    }
-
-    // Sets the map on all markers in the array.
-    function setMapOnAll(map) {
-      for (var i = 0; i < markers.length; i++) {
-        markers[i].setMap(map);
-      }
-    }
 
     function click() {
         addMarker(locate); 
         marker.setVisible(false);
+        infowindow.close();
+        check_number();
     }
-    document.getElementById('btn-add').onclick = click;
+  document.getElementById('btn-add').onclick = click;
 }
 
+// Adds a marker to the map and push to the array.
+function addMarker(location) {
+  var marker = new google.maps.Marker({
+    position: location,
+    label: labels[labelIndex++ % labels.length],
+    animation: google.maps.Animation.DROP,
+    draggable: true,
+    title: place.name,
+    map: map
+  });
+  markers.push(marker);
+  marker.addListener('click', toggleBounce);
+  info_for_markers.push(place.name);
+  address_for_markers.push(address);
+  var infowin = new google.maps.InfoWindow();
+  infowin.setContent('<div><strong>' + info_for_markers.pop() + '</strong><br>' + address_for_markers.pop());
+  infowin.open(map, marker);
+  console.log(marker.position.lat() +" "+marker.position.lng() );
+    if(markers.length>1)   
+    {
+        waypts.push({ location });
+    }
+}
+
+//
+function toggleBounce() {
+  if (marker.getAnimation() !== null) {
+    marker.setAnimation(null);
+  } else {
+    marker.setAnimation(google.maps.Animation.BOUNCE);
+  }
+}
+
+// Sets the map on all markers in the array.
+function setMapOnAll(map) {
+  for (var i = 0; i < markers.length; i++) {
+    markers[i].setMap(map);
+  }
+}
+
+//Draw ways
+function calculateAndDisplayRoute(directionsService, directionsDisplay, start, finish) {
+  directionsService.route({
+    origin: {lat: start.position.lat(), lng: start.position.lng()},  
+    destination: {lat: finish.position.lat(), lng: finish.position.lng()},
+    waypoints: waypts,
+    // Note that Javascript allows us to access the constant
+    // using square brackets and a string value as its
+    // "property."
+    travelMode: google.maps.TravelMode.DRIVING
+  }, function(response, status) {
+    if (status == google.maps.DirectionsStatus.OK) {
+        console.log(response);
+      directionsDisplay.setDirections(response);
+    } else {
+      window.alert('Directions request failed due to ' + status);
+    }
+  })
+}
+
+function check_number() {
+    if(markers.length>1)
+    {
+        calculateAndDisplayRoute(directionsService, directionsDisplay, markers[0], markers[markers.length-1]);
+    }
+}
 
 
